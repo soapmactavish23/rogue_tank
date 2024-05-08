f304765b4b4b058d75b214d2bdd1e7b1ab01a232
suporte <ricknogueira1231@gmail.com>
2024-05-08 15:56:39 -0300

    Controles e input do movimento horizontal do tanque

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -1,10 +1,20 @@
 extends KinematicBody2D
 
+var speed = 100
 
 func _ready():
 	pass
 	
 func _process(delta):
 	
-	translate(Vector2(10, 0) * delta)
+	var dir_x = 0
 	
+	if Input.is_action_pressed("ui_right"):
+		dir_x += 1
+	if Input.is_action_pressed("ui_left"):
+		dir_x -= 1
+	
+	
+	translate(Vector2(dir_x, 0) * delta * speed)
+	
+	pass
