2dd17063f8bfa9d317c53f13f765f5bebf4e31ec
suporte <ricknogueira1231@gmail.com>
2024-05-08 16:05:30 -0300

    Controle e input do movimento vertical do tanque

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -8,13 +8,18 @@ func _ready():
 func _process(delta):
 	
 	var dir_x = 0
+	var dir_y = 0
 	
 	if Input.is_action_pressed("ui_right"):
 		dir_x += 1
 	if Input.is_action_pressed("ui_left"):
 		dir_x -= 1
+	if Input.is_action_pressed("ui_up"):
+		dir_y -= 1
+	if Input.is_action_pressed("ui_down"):
+		dir_y += 1
 	
 	
-	translate(Vector2(dir_x, 0) * delta * speed)
+	translate(Vector2(dir_x, dir_y) * delta * speed)
 	
 	pass
