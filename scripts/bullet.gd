297e1a325ce2488a0bbb38ac9e07a7975ee41ade
soapmactavish23 <ricknogueira1231@gmail.com>
2024-05-09 00:01:51 -0300

    Criando o fogo de disparo da torreta do canhão

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var vel = 400
-var dir = Vector2(0, -1)
+var dir = Vector2(0, -1) setget set_dir
 
 func _ready():
 	pass # Replace with function body.
@@ -10,3 +10,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	translate(dir * vel * delta)
+
+
+func _on_notifier_screen_exited():
+	queue_free()
+	pass
+
+func set_dir(val):
+	dir = val
+	rotation = dir.angle()
+	pass
--- scripts/tank.gd
+++ scripts/tank.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed = 100
+var speed = 150
 var pre_bullet = preload("res://scenes/bullet.tscn")
 
 func _ready():
@@ -21,10 +21,16 @@ func _process(delta):
 		dir_y += 1
 	
 	if Input.is_action_just_pressed(("ui_shoot")):
-		print("boom")
-		var bullet = pre_bullet.instance()
-		bullet.global_position = $barrel/muzzle.global_position
-		get_parent().add_child(bullet)
+		if get_tree().get_nodes_in_group("cannon_bullets").size() < 6:
+			var bullet = pre_bullet.instance()
+			bullet.global_position = $barrel/muzzle.global_position
+			
+			bullet.dir = Vector2(cos(rotation), sin(rotation)).normalized()
+			
+			get_parent().add_child(bullet)
+		
+	look_at(dir_x)
+		
 	
 	
 	translate(Vector2(dir_x, dir_y) * delta * speed)
