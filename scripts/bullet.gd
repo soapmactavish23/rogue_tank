f508bc0c8db4ab6814b33a34253ae23aedd0b6f8
soapmactavish23 <ricknogueira1231@gmail.com>
2024-05-10 21:59:59 -0300

    Efeito na autodestruição do projétil

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,16 +1,30 @@
 extends Area2D
 
+const MAX_DIST = 100
+
 var vel = 400
 var dir = Vector2(0, -1) setget set_dir
+onready var init_pos = global_position
+
+var live = true
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	translate(dir * vel * delta)
-
+	
+	if live:
+		if global_position.distance_to(init_pos) >= MAX_DIST:
+			$smoke.emitting = false
+			live = false
+			$sprite.visible = false
+			$anim_self_destruction.play("explode")
+			monitoring = false
+			monitorable = false
+			yield($anim_self_destruction, "animation_finished")
+			queue_free()
+	
+		translate(dir * vel * delta)
 
 func _on_notifier_screen_exited():
 	queue_free()
