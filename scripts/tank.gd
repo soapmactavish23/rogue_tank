17fe0e5aa8f5f8f870370f8e15a1fa7dad176c84
soapmactavish23 <ricknogueira1231@gmail.com>
2024-05-18 18:03:03 -0300

    Movimentando a torreta do tanque com o ponteir

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -1,13 +1,13 @@
 tool
 extends KinematicBody2D
 
-var BULLET_TANK_GROUP = "bullet-" + str(self)
-
 # PI = 180 graus
 const ROT_VEL = PI / 2
+const MAX_SPEED = 150
 
-var speed = 150
+var acell = 0
 var pre_bullet = preload("res://scenes/bullet.tscn")
+var BULLET_TANK_GROUP = "bullet-" + str(self)
 
 export(int, "green", "bigRed", "blue", "dark", "darkLarge", "huge", "sand") var body = 0 setget set_body
 export(int, "green", "bigRed", "blue", "dark", "darkLarge", "huge", "sand") var barrel = 0 setget set_barrel
@@ -42,35 +42,20 @@ func _physics_process(delta):
 	
 	if Engine.editor_hint:
 		return
-	
-#	var dir_x = 0
-#	var dir_y = 0
-#
-#	if Input.is_action_pressed("ui_right"):
-#		dir_x += 1
-#	if Input.is_action_pressed("ui_left"):
-#		dir_x -= 1
-#	if Input.is_action_pressed("ui_up"):
-#		dir_y -= 1
-#	if Input.is_action_pressed("ui_down"):
-#		dir_y += 1
-#
-#	if Input.is_action_just_pressed(("ui_shoot")):
-#
-#		if get_tree().get_nodes_in_group(BULLET_TANK_GROUP).size() < 6:
-#			var bullet = pre_bullet.instance()
-#			bullet.global_position = $barrel/muzzle.global_position
-#			$barrel/fx.play()
-#
-#			bullet.dir = Vector2(cos(rotation), sin(rotation)).normalized()
-#			bullet.add_to_group(BULLET_TANK_GROUP)
-#			$barrel/anim.play("fire")
-#			get_parent().add_child(bullet)
-#
-#	look_at(get_global_mouse_position())
-#	move_and_slide(Vector2(dir_x, dir_y) * speed)
+		
+	if Input.is_action_just_pressed(("ui_shoot")):
+		if get_tree().get_nodes_in_group(BULLET_TANK_GROUP).size() < 6:
+			var bullet = pre_bullet.instance()
+			bullet.global_position = $barrel/muzzle.global_position
+			$barrel/fx.play()
+
+			bullet.dir = Vector2(cos($barrel.global_rotation), sin($barrel.global_rotation)).normalized()
+			bullet.add_to_group(BULLET_TANK_GROUP)
+			$barrel/anim.play("fire")
+			get_parent().add_child(bullet)
 
 	var rot = 0
+	var dir = 0
 
 	if Input.is_action_pressed("ui_right"):
 		rot += 1
@@ -79,8 +64,24 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		rot -= 1
 		pass
+	
+	if Input.is_action_pressed("ui_up"):
+		dir += 1
+	
+	if Input.is_action_pressed("ui_down"):
+		dir -= 1
 		
 	rotate(ROT_VEL * rot * delta)
+	
+	
+	if dir != 0:
+		acell = lerp(acell, MAX_SPEED, .01)
+	else:
+		acell = lerp(acell, 0, .05)
+	
+	move_and_slide(Vector2(cos(rotation), sin(rotation)) * dir * acell)
+	
+	$barrel.look_at(get_global_mouse_position())
 
 func set_body(val):
 	body = val
