9fc1800d5546355a8fff9727d9a864f86ccffb94
soapmactavish23 <ricknogueira1231@gmail.com>
2024-05-25 23:50:09 -0300

    42 - Física dos fragmentos

---
+++ scripts/fragment.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+export var boncing = 0.3
+
+func _ready():
+	bounce = boncing
+	gravity_scale = 0
+	linear_damp = 1
+	angular_velocity = randf() * 1
+	var dir = randf() * (PI * 2)
+	apply_impulse(Vector2(), Vector2(cos(dir), sin(dir)) * 200)
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,14 +1,5 @@
 extends Node
 
-var score = 0 setget set_score
-
 func _ready():
+	randomize()
 	pass
-
-
-func add_score(val):
-	score += val
-	print(score)
-	
-func set_score(val):
-	print("cant write score. use funciont add_score")
