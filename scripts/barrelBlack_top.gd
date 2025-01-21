8fb57335d06a58db194a6c7136d0f30ea5b2ce6f
Henrick <ricknogueira1231@gmail.com>
2025-01-20 23:49:25 -0300

    54. 50 - Espalhando oleo pela pista

--- scripts/barrelBlack_top.gd
+++ scripts/barrelBlack_top.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+const PRE_OIL = preload("res://scenes/oilSpill_large.tscn")
+
 func _ready():
 	$area_obastacle.connect("hitted", self, "on_area_hitted")
 	$area_obastacle.connect("destroid", self, "on_area_destroid")
@@ -9,4 +11,18 @@ func on_area_hitted(damage, health, node):
 	pass
 
 func on_area_destroid():
+	
+	var oil_count = rand_range(3, 8)
+	
+	for o in range(oil_count):
+		var oil = PRE_OIL.instance()
+		var angle = randf() * (PI * 2)
+		oil.global_position = global_position + Vector2(cos(angle), sin(angle)).normalized() * rand_range(30, 60)
+		$'../'.add_child(oil)
+	
+	$area_obastacle.queue_free()
+	$shape.queue_free()
+	$anim.play("explode")
+	yield($anim, "animation_finished")
+	
 	queue_free()
