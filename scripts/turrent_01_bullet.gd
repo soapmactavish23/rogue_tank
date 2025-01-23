2a826985ffd04b2dfa2fb9fa4c63cb1518ae4e93
Henrick <ricknogueira1231@gmail.com>
2025-01-23 20:34:44 -0300

    61. 57 - Disparos da torreta

---
+++ scripts/turrent_01_bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var dir = Vector2() setget set_dir
+var vel = 400
+var max_dist = 300
+onready var init_pos = global_position
+
+func _ready():
+	#self.dir = Vector2(1, -1)
+	pass
+
+func _physics_process(delta):
+	translate(dir * vel * delta)
+	if global_position.distance_to(init_pos) > max_dist:
+		queue_free()
+	
+func set_dir(val):
+	dir = val
+	rotation = val.angle()
--- scripts/turret_01.gd
+++ scripts/turret_01.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 var bodies = []
 var rot_vel = PI * .2
 
+const PRE_BULLET = preload("res://scenes/turrent_01_bullet.tscn")
+
 func _process(delta):
 	if bodies.size():
 		var angle = $cannon.get_angle_to(bodies[0].global_position)
@@ -19,3 +21,10 @@ func _on_sensor_body_exited(body):
 	var index = bodies.find(body)
 	if index >= 0:
 		bodies.remove(index)
+
+
+func _on_shoot_timer_timeout():
+	var bullet = PRE_BULLET.instance()
+	bullet.global_position = global_position
+	bullet.dir = Vector2(cos($cannon.rotation), sin($cannon.rotation))
+	get_parent().add_child(bullet)
