f39a4ed2d9c521a652da6ace77fe478ac2e85e37
Henrick <ricknogueira1231@gmail.com>
2025-01-24 20:46:01 -0300

    62. 58 - Mira lazer da torreta

--- scripts/turret_01.gd
+++ scripts/turret_01.gd
@@ -10,21 +10,27 @@ func _process(delta):
 		var angle = $cannon.get_angle_to(bodies[0].global_position)
 		if abs(angle) > .1:
 			$cannon.rotation += rot_vel * delta * sign(angle)
-		#$cannon.look_at(bodies[0].global_position)
-
-
+	
 func _on_sensor_body_entered(body):
 	if(body is KinematicBody2D):
+		if !bodies.size():
+			$shoot_timer.start()
 		bodies.append(body)
+		$cannon/sight.enabled = true
+		
 
 func _on_sensor_body_exited(body):
 	var index = bodies.find(body)
 	if index >= 0:
 		bodies.remove(index)
+	if !bodies.size():
+		$cannon/sight.enabled = false
+		$shoot_timer.stop()
 
 
 func _on_shoot_timer_timeout():
-	var bullet = PRE_BULLET.instance()
-	bullet.global_position = global_position
-	bullet.dir = Vector2(cos($cannon.rotation), sin($cannon.rotation))
-	get_parent().add_child(bullet)
+	if $cannon/sight.is_colliding():
+		var bullet = PRE_BULLET.instance()
+		bullet.global_position = global_position
+		bullet.dir = Vector2(cos($cannon.rotation), sin($cannon.rotation))
+		get_parent().add_child(bullet)
