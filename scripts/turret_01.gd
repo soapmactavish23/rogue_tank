4b2f3cde2551d159d7c7cd42df4acf024ee54585
Henrick <ricknogueira1231@gmail.com>
2025-01-25 23:20:42 -0300

    63. 59 - Torreta troca de alvo pela mira lzaer

--- scripts/turret_01.gd
+++ scripts/turret_01.gd
@@ -10,6 +10,12 @@ func _process(delta):
 		var angle = $cannon.get_angle_to(bodies[0].global_position)
 		if abs(angle) > .1:
 			$cannon.rotation += rot_vel * delta * sign(angle)
+	if $cannon/sight.is_colliding():
+		if $cannon.get_collider() != bodies[0]:
+			var oldBody = bodies[0]
+			var newBodyIndex = bodies.find($cannon/sight.get_collider())
+			bodies[0] = $cannon/sight.get_collider()
+			bodies[newBodyIndex] = oldBody
 	
 func _on_sensor_body_entered(body):
 	if(body is KinematicBody2D):
