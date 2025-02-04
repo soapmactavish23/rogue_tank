4b803e41c3cfd234d3529f72351b20b51f31a4be
Henrick <ricknogueira1231@gmail.com>
2025-02-04 20:51:51 -0300

    80. 76 - Radar circular 2

--- scripts/radar_circle.gd
+++ scripts/radar_circle.gd
@@ -6,6 +6,8 @@ export(float, 10, 1000) var radius = 10 setget set_radius
 export(Color) var color = Color(0, .6, 0, .5) setget set_color
 export(NodePath) var tank
 
+onready var rate = radius / sensor_radius
+
 func _ready():
 	if tank:
 		tank = get_node(tank)
@@ -14,7 +16,16 @@ func _draw():
 	draw_circle(Vector2(), radius, color)
 	
 	if !Engine.editor_hint:
-		draw_circle(Vector2(), 3, Color(1, 1, 0, 1))
+		if tank:
+			draw_circle(Vector2(), 3, Color(1, 1, 0, 1))
+			for re in get_tree().get_nodes_in_group("radar_entity"):
+				var angle = (tank.global_position - re.global_position).angle()
+				var distance = tank.global_position.distance_to(re.global_position)
+				if distance < sensor_radius:
+					draw_circle(Vector2(cos(angle), sin(angle)) * distance * rate * -1, 2, Color(1, 1, 1, 1))
+		else:
+			print('tank not defined')
+		
 	
 func set_radius(val):
 	radius = val
@@ -25,3 +36,7 @@ func set_color(val):
 	color = val
 	if Engine.editor_hint:
 		update()
+
+
+func _on_timer_update_timeout():
+	update()
